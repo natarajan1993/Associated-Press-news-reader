76b6d8fb8f4a9da3647cfc9c6cf7ec52f1c12124
natarajan1993 <natraj_93@yahoo.com>
2017-04-07 18:39:15 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 """A rough implementation of an Alexa skill using flask-ask, beautiful soup and scikit-learn. The machine learning
-   algorithm used for updating the user preferences is called tf-idf (Term Frequency-Inverse Document Frequency). It
+   algorithm used for updating the user preferences is called tf-idf (Term Frequency-Inverse Document Frequency. It
    looks through a set of documents and finds the most common words among all the documents and ranks them by their
    frequency scores. Right now the machine learning itself is very slow because it's trawling through a set of docs on
    the local machine. More development is needed to optimize and streamline the process. Each document contains a single
@@ -17,6 +17,7 @@
    2. Use the text from the top 3 headlines in 5 separate preferences to learn the user's preferences and update
    the in-built preference accordingly. The user can set initial preferences
    3. Play an archival recording of the AP's coverage of the 1985 Challenger disaster
+   4. Summarize a headline's text
 
    Developed by:
    Natarajan Mahalingam
@@ -27,12 +28,17 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from flask import Flask, render_template
-from flask_ask import Ask, question, statement, audio
+from flask_ask import Ask, question, statement, audio, session
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 import os
 from fnmatch import fnmatch
+from sumy.parsers.html import HtmlParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
 
 app = Flask(__name__)
 ask = Ask(app, '/alexa')
@@ -42,6 +48,7 @@ ask = Ask(app, '/alexa')
 
 @ask.launch
 def new_ask():
+    clear_contents(r'C:\Users\Natarajan\PycharmProjects\AP_scraper\docs\links.txt')
     clear_everything()
     welcome = render_template('welcome')
     reprompt = render_template('reprompt')
@@ -109,29 +116,7 @@ def update_tfidf():
 def parse_word(word):
     word_list = word.split(' ')
     reshaped_word = '+'.join(word_list)
-    all_heads = scrape_simple(reshaped_word)
-    return all_heads
-
-
-'''Function used for a single search term intent. Scrapes the searched page and returns the headlines for the
-   searched term'''
-
-
-def scrape_simple(word):
-    full_list = []
-    ap = 'http://hosted.ap.org/dynamic/external/search.hosted.ap.org/wireCoreTool/Search?SITE=AP&SECTION=HOME' \
-         '&TEMPLATE=DEFAULT&query='
-
-    page = urlopen(ap + word)  # Add the url and word to search and open using urllib's urlopen function
-
-    soup = BeautifulSoup(page, 'lxml')  # 'Soupify' the page using beautiful soup. easier to search for using BS again
-
-    all_heads = soup.find_all('span', {'class': 'latestnews'})  # The AP uses a span tag with class latestnews for their
-    # headlines
-
-    for heads in all_heads:
-        full_list.append(heads.get_text())
-    return full_list
+    return reshaped_word
 
 
 '''Function used to scrape both the headline and its href link. This function is used as part of the recommender system.
@@ -155,6 +140,24 @@ def scrape(word):
     for heads in all_heads:
         full_list.append(heads.get_text())
     return full_list, all_links
+
+
+'''Function that returns all the links corresponding to the searched topic'''
+
+
+def scrape_links(word):
+    all_links = []
+    ap = 'http://hosted.ap.org/dynamic/external/search.hosted.ap.org/wireCoreTool/Search?SITE=AP&SECTION=HOME' \
+         '&TEMPLATE=DEFAULT&query='
+
+    page = urlopen(ap + word)
+
+    soup = BeautifulSoup(page, 'lxml')
+
+    for a_tag in soup.find_all('a', href=True):
+        if a_tag['href'].startswith('/dynamic/stories/'):  # This step is needed to weed out the unwanted links
+            all_links.append('http://hosted.ap.org' + a_tag['href'])
+    return all_links
 
 
 '''Function that clears all the contents of all the files in all the directories'''
@@ -217,6 +220,83 @@ def return_full_news_string(ap):
     return a
 
 
+'''Function that returns the index of the headline we want summarized'''
+
+
+def word_to_num(position):
+    index = 0
+    if position == '1st' or position == 'first':
+        index = 0
+    if position == '2nd' or position == 'second':
+        index = 1
+    if position == '3rd' or position == 'third':
+        index = 2
+    if position == '4th' or position == 'fourth':
+        index = 3
+    if position == '5th' or position == 'fifth':
+        index = 4
+    if position == '6th' or position == 'sixth':
+        index = 5
+    if position == '7th' or position == 'seventh':
+        index = 6
+    if position == '8th' or position == 'eighth':
+        index = 7
+    if position == '9th' or position == 'ninth':
+        index = 8
+    if position == '10th' or position == 'tenth':
+        index = 9
+    if position == '11th' or position == 'eleventh':
+        index = 10
+    if position == '12th' or position == 'twelfth':
+        index = 11
+    if position == '13th' or position == 'thirteenth':
+        index = 12
+    if position == '14th' or position == 'fourteenth':
+        index = 13
+    if position == '15th' or position == 'fifteenth':
+        index = 14
+    if position == '16th' or position == 'sixteenth':
+        index = 15
+    if position == '17th' or position == 'seventeeth':
+        index = 16
+    if position == '18th' or position == 'eighteenth':
+        index = 17
+    if position == '19th' or position == 'nineteenth':
+        index = 18
+    if position == '20th' or position == 'twentieth':
+        index = 19
+    if position == '21st' or position == 'twenty first':
+        index = 20
+    if position == '22nd' or position == 'twenty second':
+        index = 21
+    if position == '23rd' or position == 'twenty third':
+        index = 22
+    if position == '24th' or position == 'twenty fourth':
+        index = 23
+    if position == '25th' or position == 'twenty fifth':
+        index = 23
+
+    return index
+
+
+'''Function that summarizes the text. Not my work. Used a pre-existing text summarizer from online'''
+
+
+def summarize_text(url):
+    LANGUAGE = "english"
+    SENTENCES_COUNT = 10
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+    full_sentence_list = []
+
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        full_sentence_list.append(str(sentence))
+    return full_sentence_list
+
+
 '''Function that clears the recommendations text file and updates it using the top 5 results from the tfidf algorithm'''
 
 
@@ -225,6 +305,37 @@ def update_recommendations(headlines_list):
     with open(r'C:\Users\Natarajan\PycharmProjects\AP_scraper\docs\topics.txt', 'w') as file:
         file.writelines("\n".join(headlines_list))
 
+
+'''Function that writes all the links of a topic to a temporary file'''
+
+
+def write_links_to_file(links_list, path=r'C:\Users\Natarajan\PycharmProjects\AP_scraper\news_temp.txt'):
+    with open(path, 'w') as file:
+        file.write("\n".join(links_list))
+
+
+'''Flask ask function that returns the summary of the headline to the device'''
+
+
+@ask.intent('SummarizeIntent')
+def summarize(position):
+    all_links = []
+    if position is None:
+        reprompt = render_template('reprompt_summary')
+        return question(reprompt)
+    else:
+        position_num = word_to_num(position)
+        with open(r'C:\Users\Natarajan\PycharmProjects\AP_scraper\news_temp.txt', 'r') as file:
+            for line in file:
+                all_links.append(line)
+        url = all_links[position_num]
+        url = url.replace(' ', '')[:-1]
+        summary = summarize_text(url)
+        summary_message = '... '.join(summary)
+        message = render_template('summary')
+        return statement(message + '... ' + summary_message)
+
+
 '''Flask ask function that resets the preferences to the initial one. Mainly to simplify the typing'''
 
 
@@ -232,6 +343,7 @@ def update_recommendations(headlines_list):
 def reset_preferences():
     update_recommendations(['trump', 'gop', 'military', 'budget', 'new york'])
     return statement('Preferences have been reset')
+
 
 '''Flask ask function that returns the list from the recommended topics text file'''
 
@@ -246,6 +358,9 @@ def return_topics():
     all_topics = '... '.join(lines)
     message = render_template('recommended_topics')
     return statement(message + '... ' + all_topics)
+
+
+'''Flask ask function for when the user asks for the recommended news'''
 
 
 @ask.intent('RecommendedNewsIntent')
@@ -283,9 +398,10 @@ def recommended_news():
         e5.append(return_full_news_string(e))
 
     write_text_to_files(e1 + e2 + e3 + e4 + e5)  # Write all the lists to their corresponding files
+    write_links_to_file(l1 + l2 + l3 + l4 + l5)
     update_tfidf()  # Probably remove
     all_recommended_headlines_list = list(set(h1[:3] + h2[:3] + h3[:3] + h4[:3] + h5[:3]))  # Adds all the headlines and
-    # removes duplicates and saves as a list
+    # removes duplicates and saves as a list. Use only first 3 headlines from each topic
     all_recommended_headlines_string = '... '.join(
         all_recommended_headlines_list)  # Converts the list to a large string
     message = render_template('recommended_news')
@@ -301,7 +417,9 @@ def search_news(word):
         reprompt_word = render_template("reprompt_word")
         return question(reprompt_word)
     else:
-        all_heads = parse_word(word)
+        new_word = parse_word(word)
+        all_heads, all_links = scrape(new_word)
+        write_links_to_file(all_links)
         latest_news_message = render_template('latest_news', word=word)
         heads = '... '.join(all_heads)
         return statement(latest_news_message + '... ' + heads + '... ')
